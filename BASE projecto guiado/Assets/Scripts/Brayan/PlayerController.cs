9552e71b5391dd29dce4e665510f32b20b88117d
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2021-11-04 11:55:45 -0500

    Modificación Array en PlayerController

--- BASE projecto guiado/Assets/Scripts/Brayan/PlayerController.cs
+++ BASE projecto guiado/Assets/Scripts/Brayan/PlayerController.cs
@@ -10,8 +10,10 @@ public class PlayerController : MonoBehaviour
     public float health = 100;
     public Text winner;
     Movement movement;
-    Gun[] guns;
+    //Gun[] guns;
+    Gun gun;
     [SerializeField] Slider healthbar;
+    
     public void Hit(float force)
     {
         health -= force;
@@ -24,7 +26,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         movement = GetComponent<Movement>();
-        guns = GetComponentsInChildren<Gun>();
+        //guns = GetComponentsInChildren<Gun>();
+        gun = GetComponentInChildren<Gun>();
     }
     void Update()
     {
@@ -32,11 +35,13 @@ public class PlayerController : MonoBehaviour
         movement.Sound();
         movement.SetLights();
         movement.Rotate();
-        for (int i = 0; i < guns.Length; i++)
+        gun.Rotate();
+        gun.Shoot();
+        /*for (int i = 0; i < guns.Length; i++)
         {
             guns[i].Rotate();
             guns[i].Shoot();
-        }      
+        }  */    
     }
     public void Heal(float recover)
     {
